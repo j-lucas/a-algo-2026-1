e12eaa32d88ec9d6164fb0e13093f8f13a381e94
j-lucas <j.lucas_skt@hotmail.com>
2026-03-19 22:50:10 -0300

    Verificação de palíndromos

---
+++ dever-03/main.py
@@ -0,0 +1,22 @@
+def eh_palindromo(arr, inicio=0, fim=None):
+    if fim is None:
+        fim = len(arr) - 1
+
+    if inicio >= fim:
+        return True
+
+    if arr[inicio] != arr[fim]:
+        return False
+
+    return eh_palindromo(arr, inicio + 1, fim - 1)
+
+
+array1 = [0, 1, 2, 3, 2, 1, 0]
+array2 = ["a", "b", "b", "a"]
+array3 = ["a", "b", "c", "b", "a"]
+array4 = ["a", "b", "c", "f", "b", "a"]
+
+print(eh_palindromo(array1))  # True
+print(eh_palindromo(array2))  # True
+print(eh_palindromo(array3))  # True
+print(eh_palindromo(array4))  # False
